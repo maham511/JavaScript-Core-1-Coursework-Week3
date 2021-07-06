7da0ea4fbb8d3fd96845b60dd899aae7977bbb19
maham511 <maha.malik511@gmail.com>
2021-07-06 10:37:55 +0100

    Finished H-->2

--- 1-exercises/H-array-methods-2/exercise2.js
+++ 1-exercises/H-array-methods-2/exercise2.js
@@ -6,9 +6,17 @@
   For example, capitailise("hello") should return "Hello"
   Tip: use the string method .split() and the array method .join()
 */
-
+//Pseudo
+//split str to substrings of letters with .split()
+//capitalise 1st letter with .toUpperCase()
+//join split letters 
 function capitalise(str) {
-    return str[0].toUpperCase();
+
+  const splitName = str.split(""); //splits name into array of letters
+  splitName[0] = splitName[0].toUpperCase(); //1st element capitalised and updated in splitName
+  const joinedCapped = splitName.join(""); //joins splitName array back into 1 word
+
+  return joinedCapped; // returns array of letters
 }
 
 /* 
