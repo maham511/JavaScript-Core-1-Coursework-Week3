96d63118b73933a70cb33a9b08be05abaae04197
maham511 <maha.malik511@gmail.com>
2021-07-07 12:18:42 +0100

    Passed mandatory 2

--- 2-mandatory/2-oxygen-levels.js
+++ 2-mandatory/2-oxygen-levels.js
@@ -11,7 +11,20 @@
     Some string methods that might help you here are .replace() and .substring(). 
 */
 
-function findSafeOxygenLevel() {}
+function findSafeOxygenLevel(strArr) {
+
+  //Predicate function checks if number in range, returns boolean
+  function isSafeLevel(level) {  
+    //1st condition: checks if string values are between 19.6 and 23.5, using string comparison
+    //2nd checks if string item has % symbol, only including items with '%'
+    //3rd checks if item has two 0s together, filters out '200%'
+  return level > "19.6" && level < "23.5" && level.includes("%") && !level.includes('00');
+}
+
+//safeLevel works, but adds% to all numbers
+let safeLevel = strArr.find(isSafeLevel);
+return safeLevel;
+ }
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
